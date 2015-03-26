3a20178081a8c65e347fe2b2079ed84aff423d91
Zachary Waldowski <zach@waldowski.me>
2015-03-26 16:01:12 -0400

    Add project structure and targets

---
+++ Lustre/Lustre.h
@@ -0,0 +1,15 @@
+//
+//  Lustre.h
+//  Lustre
+//
+//  Created by Zachary Waldowski on 3/26/15.
+//  Copyright (c) 2015 Zachary Waldowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lustre.
+FOUNDATION_EXPORT double LustreVersionNumber;
+
+//! Project version string for Lustre.
+FOUNDATION_EXPORT const unsigned char LustreVersionString[];
