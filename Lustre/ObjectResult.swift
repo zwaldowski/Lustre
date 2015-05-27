049a434c358008849482bb1d1c2bf6908f7cb437
Zachary Waldowski <zach@waldowski.me>
2015-05-27 19:54:37 -0400

    Also privatize ObjectResult storage

--- Lustre/ObjectResult.swift
+++ Lustre/ObjectResult.swift
@@ -8,22 +8,24 @@
 
 import Foundation
 
-/// Container for a successful object (`T`) or a failure (`NSError`)
-public enum ObjectResult<T: AnyObject> {
-    case Success(T)
+private enum ObjectResultStorage {
+    case Success(AnyObject)
     case Failure(NSError)
 }
 
-extension ObjectResult: ResultType {
+/// Container for a successful object (`T`) or a failure (`NSError`).
+public struct ObjectResult<T: AnyObject>: ResultType {
+
+    private let storage: ObjectResultStorage
 
     /// Creates a result in a success state
     public init(_ object: T) {
-        self = .Success(object)
+        storage = .Success(object)
     }
 
     /// Creates a result in a failure state
     public init(failure: NSError) {
-        self = .Failure(failure)
+        storage = .Failure(failure)
     }
     
     /// Case analysis.
@@ -31,8 +33,8 @@ extension ObjectResult: ResultType {
     /// Returns the value produced by applying a given function in the case of
     /// success, or an alternate given function in the case of failure.
     public func analysis<R>(@noescape #ifSuccess: T -> R, @noescape ifFailure: NSError -> R) -> R {
-        switch self {
-        case .Success(let object): return ifSuccess(object)
+        switch storage {
+        case .Success(let object): return ifSuccess(unsafeDowncast(object))
         case .Failure(let error): return ifFailure(error)
         }
     }
@@ -90,5 +92,5 @@ public extension ObjectResult {
 
 /// A success `ObjectResult` returning a reference type `value`.
 public func success<T: AnyObject>(value: T) -> ObjectResult<T> {
-    return .Success(value)
+    return ObjectResult(value)
 }
